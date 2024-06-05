44bec80911ade700d22a2e4d80a42c1f89790626
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-04 23:18:23 -0300

    feat: add lib react hook form

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <div><p className="text-black text-center font-medium mt-5 text-2xl">Dashboard privado</p></div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
+'use client';
+
+import Head from "next/head";
+import { useForm } from "react-hook-form";
+
 export default function Home() {
+
+  const { register, handleSubmit } = useForm();
+
+  function handleSignIn(data) {
+    console.log(data)
+  }
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+
+      <Head>
+        <title>Home</title>
+      </Head>
+
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm bg-slate-700 p-10 rounded-lg drop-shadow-2xl">
         <h1 className="font-medium mb-4 text-2xl text-white">Login</h1>
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit(handleSignIn)}>
           <div>
             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-300">Email address</label>
             <div className="mt-2">
-              <input id="email" name="email" type="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input
+                {...register('email')}
+                id="email"
+                name="email"
+                type="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
           </div>
 
@@ -16,7 +40,14 @@ export default function Home() {
               <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-300">Password</label>
             </div>
             <div className="mt-2">
-              <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input
+                {...register('password')}
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
           </div>
 
