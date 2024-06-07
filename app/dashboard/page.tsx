be2caca8b7a29234860c526c54a2a14cdc30e5af
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-06 22:54:25 -0300

    feat: add avatar url

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,6 +11,7 @@ export default function Dashboard() {
             <p className="text-black text-center font-medium mt-5 text-2xl">
                 Dashboard privado
             </p>
+            <img src={user?.avatar_url} alt="avatar" />
         </div>
     );
 }
